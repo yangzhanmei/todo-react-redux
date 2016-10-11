80674cf7e447269067b56573313492dbc01d1059
yangzhanmei <15809244435@163.com>
2016-10-11 15:33:10 +0800

    init project

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return <div>
+            hello
+        </div>
+    }
+}
+
+render(<App/>, document.getElementById('app'));
